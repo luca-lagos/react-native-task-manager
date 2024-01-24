a91f2f9149164f03adfda292bb7065499b43c15a
luca-lagos <lagos.herrera.luca@gmail.com>
2024-01-23 22:54:06 -0300

    Upload first app prototype

--- App.js
+++ App.js
@@ -1,10 +1,13 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.h2}>Hola, Coder!</Text>
+      <Text style={styles.p}>
+        Esta es mi primer Mobile App en React Native :)
+      </Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +16,16 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+    gap: 10,
+  },
+  h2: {
+    fontSize: 30,
+    fontWeight: "bold",
+  },
+  p: {
+    fontStyle: "italic",
   },
 });
